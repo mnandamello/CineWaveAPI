cbde40c6135fa6a0fd0a7b49b71a34006d49fa14
mnandamello <mfernanda.nintendo@gmail.com>
2024-09-08 19:14:29 -0300

    criando as models e adicionando dependencias

---
+++ CineWaveAPI/Models/Campanha.cs
@@ -0,0 +1,14 @@
+﻿namespace CineWaveAPI.Models
+{
+    public class Campanha
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string MovieTitle { get; set; }
+        public string GenderMovie { get; set; }
+        public string AgeRange { get; set; }
+        public float Budget { get; set; }
+        public int ReachExpectations { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ CineWaveAPI/Models/Insights.cs
@@ -0,0 +1,12 @@
+﻿namespace CineWaveAPI.Models
+{
+    public class Insights
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public int RoiForecast { get; set; }
+        public string MarketingChanne { get; set; }
+        public float AverageCostPerClick { get; set; }
+        public int ConversionPrediction { get; set; }
+    }
+}
